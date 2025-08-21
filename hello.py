9e49f0c06c8ad2968c6ceb4550baf8094010877b
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-08-21 01:53:15 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,13 +15,22 @@ def titulo():
 
 @app.route('/home')
 def home():
-    return 'home'
+    return '''
+    home
+    <p><a href="/">voltar</a></p>
+    '''
 
 @app.route('/Identificacao')
 def Identificacao():
-    return '<p><b><h2>Avaliação contínua: Aula 030</h2></b></p><p><b><h3>Aluno: Ednilton S. Moraes</h3></b></p><p><b><h3>Prontuário: PT3025527</h3></b></p><p><b><h4>Instituição: IFSP</h4></b></p><p><h5>Voltar</h5></p>'
+    return '''
+    <p><b><h2>Avaliação contínua: Aula 030</h2></b></p><p><b><h3>Aluno: Ednilton S. Moraes</h3></b></p><p><b><h3>Prontuário: PT3025527</h3></b></p><p><b><h4>Instituição: IFSP</h4></b></p>
+    <p><a href="/">voltar</a></p>
+    '''
 
 @app.route('/Contexto')
 def Contexto():
-    return '<p><b><h2>Avaliação contínua: Aula 030</h2></b></p><p><b><h3>Seu navegador é: Mozilla/5.0 (Windows NT 10.0; Win 64; X64) Apple Web Kit/537.36 (KHTML, Like Gecko) Chrome/139.0.0;0</h3></b></p><p><b><h3>O IP do computador remoto é: 10;0;4;170</h3></b></p><p><b><h4>O host da aplicação é: ednilton.pythonanywhere.com</h4></b></p><p><h5>Voltar</h5></p>'
+    return '''
+    <p><b><h2>Avaliação contínua: Aula 030</h2></b></p><p><b><h3>Seu navegador é: Mozilla/5.0 (Windows NT 10.0; Win 64; X64) Apple Web Kit/537.36 (KHTML, Like Gecko) Chrome/139.0.0;0</h3></b></p><p><b><h3>O IP do computador remoto é: 10;0;4;170</h3></b></p><p><b><h4>O host da aplicação é: ednilton.pythonanywhere.com</h4></b></p>
+    <p><a href="/">voltar</a></p>
+    '''
 
